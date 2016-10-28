cdf69aeaa372b48f6181f716954362ca7f7abcad
Pat Murphy <ptmurphy@noctrl.edu>
2016-10-28 03:23:05 +0000

    deploy test

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   
   resources :gifts, only: [:new, :create, :edit, :update, :index, :destroy] do
     collection { post :import }
-    collection { post :inkind }
   end
   resources :donors, only: [:new, :create, :edit, :update, :index, :destroy] do
     collection { post :import }
