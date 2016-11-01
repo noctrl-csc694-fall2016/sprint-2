5ad682c0b4b6d671b30790d72615397df2e6a275
Jason Kryst <jasonkryst@gmail.com>
2016-11-01 03:58:19 +0000

    Progress to HS

--- app/controllers/hyper_surf_controller.rb
+++ app/controllers/hyper_surf_controller.rb
@@ -6,10 +6,25 @@
   #----------------------------------#
 
 class HyperSurfController < ApplicationController
-    def Donors
-        render 'donors'
+  def Donors
+    render 'donors'
+  end
+  
+  def All
+    @hypersurf = HyperSurf.new(hypersurf_params)
+    if params[:term].blank?
+      flash[:error] = "Search query is empty."
+    elsif params[:term].length > 3
+      flash[:error] = "Search query must be at least 3 characters long."
+    else
+      render 'all'
+    end 
+  end
+  
+  private
+    #define surf parameters accepted and required
+    def hypersurf_params
+      params.required(:term)
     end
-    def All
-        render 'all'
-    end
+
 end
