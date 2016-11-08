f952e457dbf3d7d3d265343b227373f00b4f5077
Jason Kryst <jasonkryst@gmail.com>
2016-11-08 00:47:13 +0000

    Tweaks search pages html; integrates search tests

---
+++ test/integration/hyper_search_test.rb
@@ -0,0 +1,60 @@
+#----------------------------------#
+# Hyper Search Integration Test
+# original written by: Jason K, Nov 7 2016
+# major contributions by:
+#
+#----------------------------------#
+require 'test_helper'
+
+class HyperSearchTest < ActionDispatch::IntegrationTest
+    
+  def setup
+    @donor = donors(:andy)
+    @activity = activities(:golf)
+  end
+
+  test "successful hyper search donor with results" do
+    get hyper_surf_donors_path
+    assert_template 'hyper_surf/donors'
+    get hyper_surf_donors_path(@donor), params: { search: "White" }
+    #assert_redirected_to hyper_surf_donors_path
+    assert_select "span.record-count", "1 result located for the surf term \"White\"."
+  end
+  
+  test "successful hyper search donor with no results" do
+    get hyper_surf_donors_path
+    assert_template 'hyper_surf/donors'
+    get hyper_surf_donors_path(@donor), params: { search: "ZZZZZZZZZ" }
+    assert_select "span.record-count", "0 results located for the surf term \"ZZZZZZZZZ\"."
+  end
+  
+  test "successful hyper search activity with results" do
+    get hyper_surf_activities_path
+    assert_template 'hyper_surf/activities'
+    get hyper_surf_activities_path(@activity), params: { search: "Golf" }
+    #assert_redirected_to hyper_surf_donors_path
+    assert_select "span.record-count", "1 result located for the surf term \"Golf\"."
+  end
+  
+  test "successful hyper search activity with no results" do
+    get hyper_surf_activities_path
+    assert_template 'hyper_surf/activities'
+    get hyper_surf_activities_path(@activity), params: { search: "XXX" }
+    #assert_redirected_to hyper_surf_donors_path
+    assert_select "span.record-count", "0 results located for the surf term \"XXX\"."
+  end
+  
+  test "successful hyper search all with results" do
+    get hyper_surf_all_path
+    assert_template 'hyper_surf/all'
+    get hyper_surf_all_path(@donor), params: { term: "White" }
+    assert_select "span.record-count", "1 result located for the surf term \"White\"."
+  end
+  
+  test "successful hyper search all with no results" do
+    get hyper_surf_all_path
+    assert_template 'hyper_surf/all'
+    get hyper_surf_all_path(@donor), params: { term: "XXX" }
+    assert_select "span.record-count", "0 results located for the surf term \"XXX\"."
+  end
+end
