3e46818b1c8019ad5ab803ae2c44a9aa32d3cf15
Jason Kryst <jasonkryst@gmail.com>
2016-11-10 05:22:03 +0000

    Initial development of HS

--- app/controllers/hyper_surf_controller.rb
+++ app/controllers/hyper_surf_controller.rb
@@ -15,7 +15,63 @@ class HyperSurfController < ApplicationController
   end
   
   def all
-    @all_search_results = Donor.search(params[:term]).paginate(:per_page => 5, :page => params[:page])
+    # Get Search Var
+    searchTerm = params[:term].downcase
+    # Prime a return
+    @fullResultSet = Array.new
+    
+    # Search The Donors
+    donors = Donor.all
+    donors.each do |d|
+      # Assume no match on each loop
+      resultsFound = false 
+    
+      if (((d.first_name.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.last_name.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.address.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.city.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.state.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.zip.to_s).include? searchTerm) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if (((d.email.downcase).include? searchTerm.downcase) && (!resultsFound))
+        resultsFound = true
+      end
+      
+      if resultsFound == true
+        # Primary Array
+        searchResults = Array.new(4)
+        
+        # Populate Array
+        searchResults[0] = "Donor"
+        searchResults[1] = "DON" + d.id.to_s
+        searchResults[2] = d.first_name + " " + d.last_name
+        searchResults[3] = d.id
+        
+        # Pass Into Result
+        @fullResultSet << searchResults
+      end
+    end
+    
+    # Surf All Gifts
+    
+    # Surf All Activities
   end
   
   private
