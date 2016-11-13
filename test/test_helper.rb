725d9121384df4a54e2a23af7d052abf96fe58c4
Jason Kryst <jasonkryst@gmail.com>
2016-11-13 22:09:25 +0000

    Updates testing files, comments out non-functioning tests to start with baseline.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,11 @@
+  #----------------------------------#
+  # GiftGarden Test Helper Test
+  # original written by: Jason K, Nov 13, 2016
+  # major contributions by:
+  #
+  #
+  # Referenced from sample_app of tutorial
+  #----------------------------------#
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -7,4 +15,22 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+  
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password')
+    post login_path, params: { session: { username: user.username,
+                                          password: password,
+                                          } }
+  end
 end
