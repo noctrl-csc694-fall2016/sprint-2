c8e966d9de92088acddaa2c36c7ea0afd79bfd88
Andy White <andyisu@outlook.com>
2016-11-19 17:53:48 +0000

    Starting the LYBUNT report...not finished yet!

--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,8 @@ Rails.application.routes.draw do
   post   '/trashes-trash-pdf', to: 'reports#trash_report'
   get    '/report-new-donors', to: 'reports#new_donors_setup'
   post   '/report-new-donors-pdf', to: 'reports#new_donors_pdf'
+  get    '/report-lybunt', to: 'reports#lybunt_setup'
+  post   '/report-lybunt-pdf', to: 'reports#lybunt_report'
   
   get    '/hyper-surf/all', to: 'hyper_surf#all'
   
